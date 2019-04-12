c2eef024487d09f7aecec90b06ff32fbf08189c5
jonnie <46609483+itzzjonnie@users.noreply.github.com>
2019-04-12 15:05:58 -0500

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,11 +1,10 @@
-from gpiozero import Robot
+from gpiozero import Motor
 from time import sleep
 
-robot = Robot(left=(4, 14), right=(17, 18))
+motor = Motor(forward=4, backward=14)
 
-for i in range(4):
-    robot.forward()
-    sleep(10)
-    robot.right()
-    sleep(1)
-   
+while True:
+    motor.forward()
+    sleep(5)
+    motor.backward()
+    sleep(5)
